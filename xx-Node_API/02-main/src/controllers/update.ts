a17d74a23254d9898761a6d7569336b0cba9e20d
김영훈 <joseph0926@naver.com>
2023-04-02 09:52:07 +0900

    update_2023.04.02

---
+++ xx-Node_API/02-main/src/controllers/update.ts
@@ -0,0 +1,99 @@
+import prisma from "../db";
+
+export const getAllUpdates = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      Updates: true,
+    },
+  });
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.Updates];
+  }, []);
+
+  res.status(201).json({ message: "모든 업데이트를 성공적으로 불러왔습니다.", data: updates });
+};
+
+export const getUpdate = async (req, res) => {
+  const update = await prisma.update.findFirst({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(201).json({ message: "해당 업데이트를 성공적으로 불러왔습니다.", data: update });
+};
+
+export const createUpdate = async (req, res) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: req.body.id,
+    },
+  });
+  if (!product) {
+    res.status(401).json({ message: "제품을 불러오는데 실패하였습니다,,," });
+  }
+
+  const update = await prisma.update.create({
+    data: req.body,
+  });
+  res.status(201).json({ message: "해당 업데이트를 성공적으로 생성하였습니다.", data: update });
+};
+
+export const updateUpdate = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      Updates: true,
+    },
+  });
+
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.Updates];
+  }, []);
+
+  const match = updates.find((update) => update.id === req.params.id);
+  if (!match) {
+    res.status(401).json({ message: "제품을 불러오는데 실패하였습니다,,," });
+  }
+
+  const updatedUpdate = await prisma.update.update({
+    where: {
+      id: req.parmas.id,
+    },
+    data: req.body,
+  });
+
+  res.status(201).json({ message: "해당 업데이트를 성공적으로 업데이트하였습니다.", data: updatedUpdate });
+};
+
+export const deleteUpdate = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      Updates: true,
+    },
+  });
+
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.Updates];
+  }, []);
+
+  const match = updates.find((update) => update.id === req.params.id);
+  if (!match) {
+    res.status(401).json({ message: "제품을 불러오는데 실패하였습니다,,," });
+  }
+
+  const deletedUpdate = await prisma.update.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(201).json({ message: "해당 업데이트를 성공적으로 삭제하였습니다.", data: deletedUpdate });
+};
--- xx-Node_API/02-main/src/router.ts
+++ xx-Node_API/02-main/src/router.ts
@@ -1,13 +1,8 @@
 import express from "express";
 import { body } from "express-validator";
 import { inputErrorHandler } from "./modules/errorMiddleware";
-import {
-  getAllProducts,
-  getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-} from "./controllers/product";
+import { getAllProducts, getProduct, createProduct, updateProduct, deleteProduct } from "./controllers/product";
+import { createUpdate, deleteUpdate, getAllUpdates, getUpdate, updateUpdate } from "./controllers/update";
 
 const router = express.Router();
 
@@ -16,40 +11,32 @@ const router = express.Router();
  */
 router.get("/product", getAllProducts);
 router.get("/product/:id", getProduct);
-router.put(
-  "/product/:id",
-  body("name").isString(),
-  inputErrorHandler,
-  updateProduct
-);
-router.post(
-  "/product",
-  body("name").isString(),
-  inputErrorHandler,
-  createProduct
-);
+router.put("/product/:id", body("name").isString(), inputErrorHandler, updateProduct);
+router.post("/product", body("name").isString(), inputErrorHandler, createProduct);
 router.delete("/product/:id", deleteProduct);
 
 /**
  * Update
  */
 
-router.get("/update");
-router.get("/update/:id");
+router.get("/update", getAllUpdates);
+router.get("/update/:id", getUpdate);
 router.put(
   "/update/:id",
   body("title").optional(),
   body("body").optional(),
   body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
-  body("version").optional()
+  body("version").optional(),
+  updateUpdate
 );
 router.post(
   "/update",
   body("title").exists().isString(),
   body("body").exists().isString(),
-  body("productId").exists().isString()
+  body("productId").exists().isString(),
+  createUpdate
 );
-router.delete("/update/:id");
+router.delete("/update/:id", deleteUpdate);
 
 // Update Point
 router.get("/updatepoint");
