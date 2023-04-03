5a353b7bf5449fda5f8d94169da04d4cea3d1ba1
김영훈 <joseph0926@naver.com>
2023-04-04 06:46:06 +0900

    fix_2023.04.04

--- 05-project-ex/controllers/auth.js
+++ 05-project-ex/controllers/auth.js
@@ -3,23 +3,53 @@ import { StatusCodes } from "http-status-codes";
 import asyncWrapper from "../middleware/async.js";
 import User from "../models/User.js";
 import BadRequestError from "../errors/bad-request.js";
+import UnauthenticatedError from "../errors/unauthenticated.js";
 
 const signup = asyncWrapper(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new BadRequestError("입력하신 값이 유효하지 않습니다, 다시한번 확인 부탁드립니다.");
+    throw new BadRequestError(
+      "입력하신 값이 유효하지 않습니다, 다시한번 확인 부탁드립니다."
+    );
   }
 
   const user = await User.create({ name, email, password });
 
   const token = user.createJWT();
 
-  res
-    .status(StatusCodes.CREATED)
-    .json({ message: "회원가입 성공!", user: { userId: user._id, username: user.name, userEmail: user.email }, token });
+  res.status(StatusCodes.CREATED).json({
+    message: "회원가입 성공!",
+    user: { userId: user._id, username: user.name, userEmail: user.email },
+    token,
+  });
 });
 
-const login = asyncWrapper(async (req, res) => {});
+const login = asyncWrapper(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError(
+      "입력하신 값이 유효하지 않습니다, 다시한번 확인 부탁드립니다."
+    );
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("유효하지 않은 인증입니다.");
+  }
+
+  const isPasswordEqule = await user.comparePassword(password);
+  if (!isPasswordEqule) {
+    throw new UnauthenticatedError("유효하지 않은 비밀번호입니다.");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    message: "로그인 성공!",
+    user: { userId: user._id, username: user.name, userEmail: user.email },
+    token,
+  });
+});
 
 const authController = {
   signup,
--- 05-project-ex/models/User.js
+++ 05-project-ex/models/User.js
@@ -34,9 +34,18 @@ UserSchema.pre("save", async function (next) {
 });
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = async function (password) {
+  const isEqule = await compare(password, this.password);
+  return isEqule;
 };
 
 export default mongoose.model("User", UserSchema);
