eddb9b020abda9c2b360fa4502475b130557baed
김영훈 <joseph0926@naver.com>
2023-04-05 06:44:39 +0900

    2023.04.05

--- 06-project-ex02/routes/auth.js
+++ 06-project-ex02/routes/auth.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+const express = require("express");
+const router = express.Router();
+const authentication = require("../middleware/authentication");
+const { register, login, updateUser } = require("../controllers/auth");
+router.post("/register", register);
+router.post("/login", login);
+router.patch("/update-user", authentication, updateUser);
 
-module.exports = router
+module.exports = router;
